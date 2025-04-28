0d45b175280420c741810c1157689eb51e9d163f
C22410612 <C22410612@mytudublin.ie>
2025-04-28 15:24:09 +0100

    Added a main menu that you can start and exit the game from

---
+++ My project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menuUI;
+    public GameObject mainCamera;
+    public GameObject cutsceneCamera;
+    public GameObject cutsceneUI;
+
+    public void StartGame()
+    {
+        // Hide the menu
+        menuUI.SetActive(false);
+        mainCamera.SetActive(false);
+
+        // Start the cutscene
+        cutsceneCamera.SetActive(true);
+        cutsceneUI.SetActive(true);
+        Debug.Log("should have played");
+
+        // Optional: Start the cutscene script
+        FindObjectOfType<CutsceneManager>().StartCutscene();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
+    }
+}
