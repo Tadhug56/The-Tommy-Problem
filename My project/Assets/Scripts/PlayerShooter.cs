cc380ef4f9a10ef472e81e68aa96095ba480a12a
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:17:44 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/PlayerShooter.cs
+++ My project/Assets/Scripts/PlayerShooter.cs
@@ -2,31 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// PlayerShooter class that handles player shooting logic
 public class PlayerShooter : MonoBehaviour
 {
-
+    // Bullet Spawning Variables
     public GameObject bulletPrefab;
     public Transform shootPoint;
 
-    public bool canShoot;
-    private float shootForce = 20.0f;
+    // Shooting Related Variables
+    public bool canShoot; // If the player has a gun
+    private float shootForce = 20.0f; // // The force of the bullet (Speed effectively)
 
-    // Update is called once per frame
+    // Every frame check if the space bar was pressed to shoot
     void Update()
     {
+        // If the space bar was pressed to shoot
         if(canShoot && Input.GetKeyDown(KeyCode.Space))
         {
-            Shoot();
+            Shoot(); // Shoot
         }
     }
 
+    // Spawn a bulelt and move it forwards with force
     void Shoot()
     {
-        GameObject projectile = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
-        Rigidbody rb = projectile.GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * shootForce;
+        GameObject projectile = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity); // Spawn the bullet
+        Rigidbody rb = projectile.GetComponent<Rigidbody>(); // Give it a rigidbody so that we can apply force to it and use it for collision checking
+        rb.velocity = transform.forward * shootForce; // Add forward velocity
 
-        // Optional: auto-destroy after a few seconds
-        Destroy(projectile, 3f);
+        Destroy(projectile, 3f); // Destroy the bullet after 3 seconds (handles if it doesn't hit anything)
     }
 }
