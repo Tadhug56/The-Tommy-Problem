ce0fc3877c1c816f2fbb17728dbfa31f13e11e58
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:16:36 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/GroundTile.cs
+++ My project/Assets/Scripts/GroundTile.cs
@@ -2,37 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// GroundTile class handles the moving planes that simulate motion
 public class GroundTile : MonoBehaviour
 {
 
     // Game pace related variables
-    public static float startingSpeed = 5.0f;
+    public static float startingSpeed = 5.0f; // Starting speed value
     public static float speed; // The speed at which the tiles will move forwards
     public float tileLength = 30.0f; // The lenth of each tile
-    private float distanceTraveled = 0f;
+    private float distanceTraveled = 0f; // How far a tile has travelled (Used to track if tile should be reset)
 
     // Player related variables
-    public Transform player;
+    public Transform player; // Player's Transform
 
+    // Set the speed at the start of the game (Specifically needed for first run of the game)
     void Start()
     {
-        speed = startingSpeed;
+        speed = startingSpeed; // Set the speed
     }
 
-    // Update is called once per frame
+    // Move the tile every frame based on speed and reset it if it's too far past
     void Update()
     {
-        float moveAmount = speed * Time.deltaTime;
+        float moveAmount = speed * Time.deltaTime; // Move speed relevant to frame time
 
         transform.position += new Vector3(moveAmount, 0, 0); // Move the tile forward across the x access
 
-        distanceTraveled += moveAmount;
+        distanceTraveled += moveAmount; // Add the distance travelled
 
         // If the tile moves its entire length accross the x access move it to the end of the queue
         if(distanceTraveled > tileLength)
         {
-            transform.position -= new Vector3(tileLength, 0, 0);
-            distanceTraveled -= tileLength;
+            transform.position -= new Vector3(tileLength, 0, 0); // Set the tile back based on its length
+            distanceTraveled -= tileLength; // Reset the distance travelled
         }
     }
 }
