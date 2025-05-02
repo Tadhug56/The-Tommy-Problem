2ab25ff62459d56907fdb64c6cca91f97be3f491
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:16:42 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// MainMenu class that handles the menu logic
 public class MainMenu : MonoBehaviour
 {
+    // Start the game when the start button is pressed
     public void StartGame()
     {   
-        FindObjectOfType<Player>().ResetPlayer();
-        GroundTile.speed = GroundTile.startingSpeed;
+        FindObjectOfType<Player>().ResetPlayer(); // Reset the player
+        GroundTile.speed = GroundTile.startingSpeed; // Reset the speed
         FindObjectOfType<CutsceneManager>().StartOpeningCutscene();// Start the opening cutscene
     }
 
+    // Exit the game
     public void ExitGame()
     {
-        Debug.Log("Exiting Game...");
-        Application.Quit();
+        Application.Quit(); // Quit the application
     }
 }
