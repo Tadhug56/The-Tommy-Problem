84567cbb5cbee1a44ae48490aaa069cbd26e8696
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:17:39 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// PlayerMovement Script
 public class PlayerMovement : MonoBehaviour
 {
+    // Stat Variables
+    private float speed = 5.0f; // Movespeed
+    private float moveRange = 4.5f; // Move range (Stay in the straight)
 
-    private float speed = 5.0f;
-    private float moveRange = 4.5f;
+    // Input variables
+    private float input; // Input
 
-    private float input;
-    private Vector3 startPosition;
-    private Player playerScript;
+    // Transform Variables
+    private Vector3 startPosition; // The start position of the player
 
-    // Start is called before the first frame update
+    // Reference Variables
+    private Player playerScript; // Player script
+
+    // Set the start position and get the Player script
     void Start()
     {
-        startPosition = transform.position;
-        playerScript = FindObjectOfType<Player>();
+        startPosition = transform.position; //Set the start position
+        playerScript = FindObjectOfType<Player>(); // Get the Player script
     }
 
-    // Update is called once per frame
+    // Every frame take in the input
     void Update()
     {
+        // If not in a cutscene / menu
         if(playerScript.playable)
         {
-            input = Input.GetAxis("Horizontal");
+            input = Input.GetAxis("Horizontal"); // Get horizontal input (A - D)
 
-            Vector3 newPosition = transform.position + new Vector3(0f, 0f, input * speed * Time.deltaTime);
+            Vector3 newPosition = transform.position + new Vector3(0f, 0f, input * speed * Time.deltaTime); // Calculate the new position based on input
 
-            newPosition.z = Mathf.Clamp(newPosition.z, startPosition.z - moveRange, startPosition.z + moveRange);
+            newPosition.z = Mathf.Clamp(newPosition.z, startPosition.z - moveRange, startPosition.z + moveRange); // Clamp it so that it's smooth (Chatgpt solution)
 
-            transform.position = newPosition;
+            transform.position = newPosition; // Set the new position
         }
     }
 }
