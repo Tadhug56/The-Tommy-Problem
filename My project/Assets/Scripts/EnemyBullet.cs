0d066c2427adf3b80991d4f4cfd5d63150a53714
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:14:48 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/EnemyBullet.cs
+++ My project/Assets/Scripts/EnemyBullet.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 
+// EnemyBullet class to handle enemybullet specific logic
 public class EnemyBullet : MonoBehaviour
-{
-    Animator animator;
+{   
+    // If the bullet hits the player they lose a health point
     void OnTriggerEnter(Collider other)
     {       
+        // If the collider is a player
         if(other.CompareTag("Player"))
         {
-            Player playerScript = other.GetComponent<Player>();
-            playerScript.health -= 1;
-            playerScript.UpdateHealthUI();
-        
+            Player playerScript = other.GetComponent<Player>(); // Get the required script
+            playerScript.health -= 1; // Subtract the health
+            playerScript.UpdateHealthUI(); // Update the UI with the new value
         }
     }
 }
