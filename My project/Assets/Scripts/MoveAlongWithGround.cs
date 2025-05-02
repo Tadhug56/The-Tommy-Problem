408a1b8def30949cc955e58eb258ac13e0d5054e
C22410612 <C22410612@mytudublin.ie>
2025-05-02 11:17:06 +0100

    Recommented for extra clarity

--- My project/Assets/Scripts/MoveAlongWithGround.cs
+++ My project/Assets/Scripts/MoveAlongWithGround.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
+// MoveAlongWithGround class that handles moving Instantiated objects along the ground to simulate motion
 public class MoveAlongWithGround : MonoBehaviour
 {
-    public float speed = GroundTile.speed;
+    public float speed = GroundTile.speed; // Tie speed to whatever speed the ground is moving out
 
+    // Every frame move the object along the ground
     void Update()
     {
-        speed = GroundTile.speed;
-        // Move strictly along world X axis
+        speed = GroundTile.speed; // Update with changes to ground speed
+
+        // Move strictly along x axis
         transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
 
-        // Destroy if beyond a certain x position
+        // If hit off the map or passed behind the player destroy the object
         if(transform.position.x > 15f || transform.position.x < -50f)
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // Destroy
         }
     }
 }
